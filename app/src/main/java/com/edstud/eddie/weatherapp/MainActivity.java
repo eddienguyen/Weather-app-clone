be72f297f155ae8eb898c3ec9b81586ac57342c6
eddienguyen <eddienguyen996@gmail.com>
2018-04-24 05:20:14 +0700

    test commit

--- app/src/main/java/com/edstud/eddie/weatherapp/MainActivity.java
+++ app/src/main/java/com/edstud/eddie/weatherapp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test comment
+        //test commit 2nd time
     }
 }
